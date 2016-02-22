0dd1b8322480f954f582e2af9d8e86c836400c62
Pau Andrio <pau.andrio@bsc.es>
2016-02-22 11:00:30 +0100

    settings.py Obtaining yaml file path from env variable

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 """Settings loader module.
 
-This module contains the different classes to read the configuration files.
-The configuration file path should be specified in the PYMDSETUP
+This module contains the classes to read the different formats of the 
+configuration files.
+
+The configuration file path should be specified in the PYMDS_CONF environment
+variable.
 
 """
 
@@ -10,8 +13,16 @@ import os
 
 
 class YamlReader(object):
-    def __init__(self, yaml_path="./conf.yaml"):
-        self.yaml_path = os.path.abspath(yaml_path)
+    """Configuration file loader for yaml format files.
+    
+    The path for the configuration file path should be provided via the
+    'PYMDS_CONF' environment variable. Default path will be './conf.yaml'
+    """
+    
+    def __init__(self):
+        self.yaml_path = os.path.abspath('./conf.yaml')
+        if os.environ.get('PYMDS_CONF') is not None:
+            self.yaml_path = os.path.abspath(os.environ.get('PYMDS_CONF'))
         self.properties = self.__read_yaml()
 
     def __read_yaml(self):
