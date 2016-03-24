1d7ec489443bee1d9597d540b9fd1f67d0ee98e1
Pau Andrio <pau.andrio@bsc.es>
2016-03-24 14:29:44 +0100

    Adding itp handling to pdb2gmx

--- test/test_grompp.py
+++ test/test_grompp.py
@@ -16,32 +16,32 @@ class TestGrompp512(unittest.TestCase):
         self.data_dir = opj(os.path.dirname(__file__), 'data')
         self.results = opj(self.data_dir, "temp_results")
 
-    def tearDown(self):
-        # Remove all files in the temp_results directory
-        for the_file in os.listdir(self.results):
-            file_path = opj(self.results, the_file)
-            try:
-                # Not removing directories
-                if os.path.isfile(file_path) and not the_file == 'README.txt':
-                    os.unlink(file_path)
-            except Exception, e:
-                print e
+    # def tearDown(self):
+    #     # Remove all files in the temp_results directory
+    #     for the_file in os.listdir(self.results):
+    #         file_path = opj(self.results, the_file)
+    #         try:
+    #             # Not removing directories
+    #             if os.path.isfile(file_path) and not the_file == 'README.txt':
+    #                 os.unlink(file_path)
+    #         except Exception, e:
+    #             print e
+    #
+    #     for the_file in os.listdir(os.getcwd()):
+    #         file_path = opj(os.getcwd(), the_file)
+    #         try:
+    #             # Not removing directories
+    #             if os.path.isfile(file_path) and the_file.startswith('#'):
+    #                 os.unlink(file_path)
+    #         except Exception, e:
+    #             print e
 
-        for the_file in os.listdir(os.getcwd()):
-            file_path = opj(os.getcwd(), the_file)
-            try:
-                # Not removing directories
-                if os.path.isfile(file_path) and the_file.startswith('#'):
-                    os.unlink(file_path)
-            except Exception, e:
-                print e
-
-    def test_launch_returns_correct_data(self):
+    def test_launch_ions(self):
         mdp_path = opj(self.data_dir, 'gmx_full_ions.mdp')
         gro_path = opj(self.data_dir, 'solvate512_gold.gro')
         top_path = opj(self.data_dir, 'solvate512_gold.top')
-        output_tpr_path = opj(self.results, 'grompp512.tpr')
-        gold_tpr_path = opj(self.data_dir, 'grompp512_gold.tpr')
+        output_tpr_path = opj(self.results, 'grompp512_ions.tpr')
+        gold_tpr_path = opj(self.data_dir, 'grompp512_ions_gold.tpr')
         output_log = opj(self.results, 'out.log')
         err_log = opj(self.results, 'err.log')
 
@@ -50,6 +50,30 @@ class TestGrompp512(unittest.TestCase):
         gpp.launch()
 
         self.assertTrue(filecmp.cmp(output_tpr_path, gold_tpr_path))
+
+    def test_launch_minimization(self):
+        mdp_path = opj(self.data_dir, 'gmx_full_min.mdp')
+        gro_path = opj(self.data_dir, 'genion512_gold.gro')
+        top_path = opj(self.data_dir, 'genion512_gold.top')
+        output_tpr_path = opj(self.results, 'grompp512_min.tpr')
+        gold_tpr_path = opj(self.data_dir, 'grompp512_min_gold.tpr')
+
+        gpp = Grompp512(mdp_path, gro_path, top_path, output_tpr_path)
+        gpp.launch()
+
+        self.assertTrue(filecmp.cmp(output_tpr_path, gold_tpr_path))
+
+    # def test_launch_nvt(self):
+    #     mdp_path = opj(self.data_dir, 'gmx_full_nvt.mdp')
+    #     gro_path = opj(self.data_dir, 'genion512_gold.gro')
+    #     top_path = opj(self.data_dir, 'genion512_gold.top')
+    #     output_tpr_path = opj(self.results, 'grompp512_min.tpr')
+    #     gold_tpr_path = opj(self.data_dir, 'grompp512_min_gold.tpr')
+    #
+    #     gpp = Grompp512(mdp_path, gro_path, top_path, output_tpr_path)
+    #     gpp.launch()
+    #
+    #     self.assertTrue(filecmp.cmp(output_tpr_path, gold_tpr_path))
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_mdrun.py
+++ test/test_mdrun.py
@@ -16,35 +16,34 @@ class TestMdrun512(unittest.TestCase):
         self.data_dir = opj(os.path.dirname(__file__), 'data')
         self.results = opj(self.data_dir, "temp_results")
 
-    def tearDown(self):
-        # Remove all files in the temp_results directory
-        for the_file in os.listdir(self.results):
-            file_path = opj(self.results, the_file)
-            try:
-                # Not removing directories
-                if os.path.isfile(file_path) and not the_file == 'README.txt':
-                    os.unlink(file_path)
-            except Exception, e:
-                print e
+    # def tearDown(self):
+    #     # Remove all files in the temp_results directory
+    #     for the_file in os.listdir(self.results):
+    #         file_path = opj(self.results, the_file)
+    #         try:
+    #             # Not removing directories
+    #             if os.path.isfile(file_path) and not the_file == 'README.txt':
+    #                 os.unlink(file_path)
+    #         except Exception, e:
+    #             print e
+    #
+    #     for the_file in os.listdir(os.getcwd()):
+    #         file_path = opj(os.getcwd(), the_file)
+    #         try:
+    #             # Not removing directories
+    #             if os.path.isfile(file_path) and the_file.startswith('#'):
+    #                 os.unlink(file_path)
+    #         except Exception, e:
+    #             print e
 
-        for the_file in os.listdir(os.getcwd()):
-            file_path = opj(os.getcwd(), the_file)
-            try:
-                # Not removing directories
-                if os.path.isfile(file_path) and the_file.startswith('#temp'):
-                    os.unlink(file_path)
-            except Exception, e:
-                print e
-
-    def test_launch_returns_correct_data(self):
+    def test_launch_minimization(self):
         tpr_path = opj(self.data_dir, 'grompp512_min_gold.tpr')
-
-        trr_path = opj(self.results, 'mdrun512.trr')
-        gold_trr_path = opj(self.data_dir, 'mdrun512_gold.trr')
-        gro_path = opj(self.results, 'mdrun512.gro')
-        gold_gro_path = opj(self.data_dir, 'mdrun512_gold.gro')
-        edr_path = opj(self.results, 'mdrun512.edr')
-        gold_edr_path = opj(self.data_dir, 'mdrun512_gold.edr')
+        trr_path = opj(self.results, 'mdrun512_min.trr')
+        gold_trr_path = opj(self.data_dir, 'mdrun512_min_gold.trr')
+        gro_path = opj(self.results, 'mdrun512_min.gro')
+        gold_gro_path = opj(self.data_dir, 'mdrun512_min_gold.gro')
+        edr_path = opj(self.results, 'mdrun512_min.edr')
+        gold_edr_path = opj(self.data_dir, 'mdrun512_min_gold.edr')
         mdr = Mdrun512(tpr_path, trr_path, gro_path, edr_path)
         mdr.launch()
 
