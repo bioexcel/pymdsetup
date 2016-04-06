5cdcaab981df5695ed3f13cc0015f3edc10620d2
Pau Andrio <pau.andrio@bsc.es>
2016-04-06 16:00:12 +0200

    Selecting just the ATOM group

--- structure/db.py
+++ structure/db.py
@@ -11,11 +11,12 @@ class MMBStructure(object):
     def __init__(self, pdb_code, output_pdb_path):
         self.pdb_code = pdb_code
         self.output_pdb_path = output_pdb_path
-        self.filter = "filter=/1"
+        self.filter = "filter=/1&group=ATOM"
 
     def get_pdb(self):
         url = ("http://mmb.irbbarcelona.org"
                "/api/pdb/"+self.pdb_code.lower()+"/coords/?"+self.filter)
+        print url
         pdb_string = requests.get(url).content
         with open(self.output_pdb_path, 'w') as pdb_file:
             pdb_file.write(pdb_string)
