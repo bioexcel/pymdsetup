47a0b1769d84ca25e2812bc38ac4fe1752d17630
Pau Andrio <pau.andrio@bsc.es>
2016-04-11 17:20:53 +0200

    creating subdirs in gromacs full workflow

--- workflows/gromacs_full.py
+++ workflows/gromacs_full.py
@@ -3,85 +3,108 @@
 
 @author: pau
 """
-
-import settings
-import requests
 import os
-import pdb2gmx
-import editconf
-import solvate
-import grompp
-import genion
-import mdrun
+from os.path import join as opj
+from pymdsetup.configuration import settings
+from pymdsetup.gromacs_wrapper import pdb2gmx
+from pymdsetup.gromacs_wrapper import editconf
+from pymdsetup.gromacs_wrapper import solvate
+from pymdsetup.gromacs_wrapper import grompp
+from pymdsetup.gromacs_wrapper import genion
+from pymdsetup.gromacs_wrapper import mdrun
+from pymdsetup.mmb_api import pdb
+from pymdsetup.mmb_api import uniprot
+from pymdsetup.scwrl_wrapper import scwrl
+
+
+def cdir(wd, name):
+    path_sp = opj(wd, name)
+    if not os.path.exists(path_sp):
+        os.makedirs(path_sp)
+        return path_sp
 
 prop = settings.YamlReader().properties
-
-pdb_path = "structure.pdb"
-pdb_code = "1NAJ"
-filter = "filter=/1"
-
-
 mdp_dir = os.path.join(os.path.dirname(__file__), 'mdp')
 
-# Get PDB file from the MMB API without solvent or ligands
-url = "mmb.irbbarcelona.org/api/pdb/"+pdb_code.lower()+"/coords/?"+filter
-pdb_string = requests.get(url)
-with open(pdb_path, 'w') as pdb_file:
-    pdb_file.write(pdb_string)
+wd = os.path.abspath(prop['workflow_path'])
+if not os.path.exists(wd):
+    os.makedirs(wd)
 
-# Create gromacs topology
-p2g = pdb2gmx.Pdb2gmx512(pdb_path, prop['p2g_gro'])
-p2g.launch()
+# Step1: mmbpdb -- Get PDB
+mmbpdb_path = cdir(wd, 'step1_mmbpdb')
+mmbpdb_pdb = opj(mmbpdb_path, prop['pdb'])
+mmbpdb = pdb.MmbPdb(prop['pdb_code'], mmbpdb_pdb)
+mmbpdb.get_pdb()
 
-# Define box dimensions
-ec = editconf.Editconf512(prop['p2g_gro'], prop['box_gro'])
-ec.launch()
+# Step2: mmbuniprot -- Get mutations
+mmbuniprot = uniprot.MmbVariants(prop['pdb_code'])
+mutations = mmbuniprot.fetch_variants()
 
-# Fill the box with water molecules
-sol = solvate.Solvate512(prop['box_gro'], prop['sol_gro'], prop['top'])
-sol.launch()
+for mut in mutations:
+    mut_path = cdir(wd, mut)
 
-# Add ions to neutralice the charge
-ions_mdp = os.path.join(mdp_dir, prop['ions_mdp'])
-gpp = grompp.Grompp512(ions_mdp, prop['sol_gro'], prop['top'],
-                       prop['ions_tpr'])
-gpp.launch()
+    # Step3: scw -- Model mutation
+    scw_path = cdir(mut_path, 'step3_scw')
+    scw_pdb = opj(scw_path, prop['mutated_pdb'])
+    scw = scwrl.Scwrl4(mmbpdb_pdb, scw_pdb, mut).launch()
 
-gio = genion.Genion512(prop['ions_tpr'], prop['ions_gro'], prop['top'])
-gio.launch()
+    # Step4: p2g -- Create gromacs topology
+    p2g_path = cdir(mut_path, 'step4_p2g')
+    p2g_gro = opj(p2g_path, prop['p2g_gro'])
+    p2g_top = opj(p2g_path, prop['p2g_top'])
+    p2g = pdb2gmx.Pdb2gmx512(scw_pdb, p2g_gro, p2g_top).launch()
 
-# Energy minimization
-gpp = grompp.Grompp512(prop['min_mdp'], prop['ions_gro'], prop['top'],
-                       prop['min_tpr'])
-gpp.launch()
+    # Define box dimensions
+    ec = editconf.Editconf512(prop['p2g_gro'], prop['box_gro'])
+    ec.launch()
 
-mdr = mdrun.Mdrun512(prop['min_tpr'], prop['min_trr'], prop['min_gro'],
-                     prop['min_edr'])
-mdr.launch()
+    # Fill the box with water molecules
+    sol = solvate.Solvate512(prop['box_gro'], prop['sol_gro'], prop['top'])
+    sol.launch()
 
-# Equilibration step 1/2 nvt (constant number of molecules, volume and temp)
-gpp = grompp.Grompp512(prop['nvt_mdp'], prop['min_gro'],  prop['top'],
-                       prop['nvt_tpr'])
-gpp.launch()
+    # Add ions to neutralice the charge
+    ions_mdp = os.path.join(mdp_dir, prop['ions_mdp'])
+    gpp = grompp.Grompp512(ions_mdp, prop['sol_gro'], prop['top'],
+                           prop['ions_tpr'])
+    gpp.launch()
 
-mdr = mdrun.Mdrun512(prop['nvt_tpr'], prop['nvt_gro'], prop['nvt_trr'],
-                     prop['nvt_edr'], output_cpt_path=prop['nvt_cpt'])
-mdr.launch()
+    gio = genion.Genion512(prop['ions_tpr'], prop['ions_gro'], prop['top'])
+    gio.launch()
 
-# Equilibration step 2/2 npt (constant number of molecules, pressure and temp)
-gpp = grompp.Grompp512(prop['npt_mdp'], prop['nvt_gro'], prop['top'],
-                       prop['npt_tpr'], cpt_path=prop['nvt_cpt'])
-gpp.launch()
+    # Energy minimization
+    gpp = grompp.Grompp512(prop['min_mdp'], prop['ions_gro'], prop['top'],
+                           prop['min_tpr'])
+    gpp.launch()
 
-mdr = mdrun.Mdrun512(prop['npt_tpr'],  prop['npt_gro'], prop['npt_trr'],
-                     prop['npt_edr'], output_cpt_path=prop['npt_cpt'])
-mdr.launch()
+    mdr = mdrun.Mdrun512(prop['min_tpr'], prop['min_trr'], prop['min_gro'],
+                         prop['min_edr'])
+    mdr.launch()
 
-# 1ns Molecular dynamics
-gpp = grompp.Grompp512(prop['md_mdp'], prop['npt_gro'], prop['top'],
-                       prop['md_tpr'],  cpt_path=prop['npt_cpt'])
-gpp.launch()
+    # Equilibration step 1/2 nvt (constant number of molecules, volume and temp)
+    gpp = grompp.Grompp512(prop['nvt_mdp'], prop['min_gro'],  prop['top'],
+                           prop['nvt_tpr'])
+    gpp.launch()
 
-mdr = mdrun.Mdrun512(prop['md_tpr'], prop['md_gro'], prop['md_trr'],
-                     prop['md_edr'], output_cpt_path=prop['md_cpt'])
-mdr.launch()
+    mdr = mdrun.Mdrun512(prop['nvt_tpr'], prop['nvt_gro'], prop['nvt_trr'],
+                         prop['nvt_edr'], output_cpt_path=prop['nvt_cpt'])
+    mdr.launch()
+
+    # Equilibration step 2/2 npt (constant number of molecules, pressure and temp)
+    gpp = grompp.Grompp512(prop['npt_mdp'], prop['nvt_gro'], prop['top'],
+                           prop['npt_tpr'], cpt_path=prop['nvt_cpt'])
+    gpp.launch()
+
+    mdr = mdrun.Mdrun512(prop['npt_tpr'],  prop['npt_gro'], prop['npt_trr'],
+                         prop['npt_edr'], output_cpt_path=prop['npt_cpt'])
+    mdr.launch()
+
+    # 1ns Molecular dynamics
+    gpp = grompp.Grompp512(prop['md_mdp'], prop['npt_gro'], prop['top'],
+                           prop['md_tpr'],  cpt_path=prop['npt_cpt'])
+    gpp.launch()
+
+    mdr = mdrun.Mdrun512(prop['md_tpr'], prop['md_gro'], prop['md_trr'],
+                         prop['md_edr'], output_cpt_path=prop['md_cpt'])
+    mdr.launch()
+
+    break
