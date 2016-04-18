f43868ee254e5a3f25d73a4a4230aadff11c82c7
scorella <scorellap@gmail.com>
2016-04-18 12:56:43 +0200

    PyCOMPSs minor changes

--- workflows/gromacs_full.py
+++ workflows/gromacs_full.py
@@ -20,7 +20,6 @@ try:
     import mmb_api.pdb as pdb
     import mmb_api.uniprot as uniprot
 except ImportError:
-    print "Error entra"
     from pymdsetup.gromacs_wrapper import pdb2gmx
     from pymdsetup.gromacs_wrapper import grompp
     from pymdsetup.scwrl_wrapper import scwrl
@@ -68,8 +67,8 @@ def main():
     mdp_dir = os.path.join(os.path.dirname(__file__), 'mdp')
     gmx_path = prop['gmx_path']
     scwrl_path = prop['scwrl4_path']
-    PyCOMPSs = sys.argv[1] == 'True'
-    print "Executing with PyCOMPSs {}".format(PyCOMPSs)
+    mdeqList = []
+    cont = 0
     wd = os.path.abspath(prop['workflow_path'])
     if not os.path.exists(wd):
         os.makedirs(wd)
@@ -88,54 +87,35 @@ def main():
         + mmbuniprot.get_uniprot() + ": No variants"
         return
 
-    print "[Sandra] NUM MUTATIONS {}".format(len(mutations))
-    mdeqList = []
-    cont = 0
     for mut in mutations:
         mut_path = cdir(wd, mut)
 
         print 'step3: scw -- Model mutation'
         scw_path = cdir(mut_path, 'step3_scw')
         scw_pdb = opj(scw_path, prop['mutated_pdb'])
-        print "[Sandra] ouput step3: {}".format(scw_pdb)
-        if not PyCOMPSs:
-            scwrl.Scwrl4(mmbpdb_pdb, scw_pdb, mut, scwrl_path=scwrl_path).launch()
-        else:
-            print "[Sandra] PyCOMPSs step3"
-            sc = scwrl.Scwrl4(mmbpdb_pdb, scw_pdb, mut, scwrl_path=scwrl_path)
-            scw_pdb2 = sc.launchPyCOMPSs()
+        scw = scwrl.Scwrl4(mmbpdb_pdb, scw_pdb, mut, scwrl_path=scwrl_path)
+        scw_pdb2 = scw.launchPyCOMPSs()
 
         print 'step4: p2g -- Create gromacs topology'
         p2g_path = cdir(mut_path, 'step4_p2g')
         p2g_gro = opj(p2g_path, prop['p2g_gro'])
         p2g_top = opj(p2g_path, prop['p2g_top'])
-        if not PyCOMPSs:
-            pdb2gmx.Pdb2gmx512(scw_pdb, p2g_gro, p2g_top, gmx_path=gmx_path, ignh=True).launch()
-        else:
-            print "[Sandra] input step4: {}".format(scw_pdb)
-            p2g = pdb2gmx.Pdb2gmx512(scw_pdb, p2g_gro, p2g_top, gmx_path=gmx_path, ignh=True)
-            p2g2 = p2g.launchPyCOMPSs(scw_pdb2)
-
+        p2g = pdb2gmx.Pdb2gmx512(scw_pdb, p2g_gro, p2g_top, gmx_path=gmx_path, ignh=True)
+        p2g2 = p2g.launchPyCOMPSs(scw_pdb2)
 
         print 'step5: ec -- Define box dimensions'
         ec_path = cdir(mut_path, 'step5_ec')
         ec_gro = opj(ec_path, prop['ec_gro'])
-        if not PyCOMPSs:
-            editconf.Editconf512(p2g_gro, ec_gro).launch()
-        else:
-            ec = editconf.Editconf512(p2g_gro, ec_gro, gmx_path=gmx_path)
-            ec2 = ec.launchPyCOMPSs(p2g2)
+        ec = editconf.Editconf512(p2g_gro, ec_gro, gmx_path=gmx_path)
+        ec2 = ec.launchPyCOMPSs(p2g2)
 
         print 'step6: sol -- Fill the box with water molecules'
         sol_path = cdir(mut_path, 'step6_sol')
         cext(p2g_path, sol_path, 'itp')
         sol_gro = opj(sol_path, prop['sol_gro'])
         sol_top = opj(sol_path, prop['sol_top'])
-        if not PyCOMPSs:
-            solvate.Solvate512(ec_gro, sol_gro, p2g_top, sol_top).launch()
-        else:
-            sol = solvate.Solvate512(ec_gro, sol_gro, p2g_top, sol_top, gmx_path=gmx_path)
-            sol2 = sol.launchPyCOMPSs(p2g2, ec2)
+        sol = solvate.Solvate512(ec_gro, sol_gro, p2g_top, sol_top, gmx_path=gmx_path)
+        sol2 = sol.launchPyCOMPSs(p2g2, ec2)
 
         print ('step7: gppions -- Preprocessing:'
                'Add ions to neutralice the charge')
@@ -145,22 +125,16 @@ def main():
         mdp_dir = '/home/compss/PyCOMPSs/git/pymdsetup/workflows/mdp/'
         shutil.copy(opj(mdp_dir, prop['gppions_mdp']), gppions_mdp)
         gppions_tpr = opj(gppions_path, prop['gppions_tpr'])
-        if not PyCOMPSs:
-            grompp.Grompp512(gppions_mdp, sol_gro, sol_top, gppions_tpr).launch()
-        else:
-            gro = grompp.Grompp512(gppions_mdp, sol_gro, sol_top, gppions_tpr, gmx_path=gmx_path)
-            gro2 = gro.launchPyCOMPSs(sol2)
+        gppions = grompp.Grompp512(gppions_mdp, sol_gro, sol_top, gppions_tpr, gmx_path=gmx_path)
+        gro2 = gppions.launchPyCOMPSs(sol2)
 
         print 'step8: gio -- Running: Add ions to neutralice the charge'
         gio_path = cdir(mut_path, 'step8_gio')
         cext(gppions_path, gio_path, 'itp')
         gio_gro = opj(gio_path, prop['gio_gro'])
         gio_top = opj(gio_path, prop['gio_top'])
-        if not PyCOMPSs:
-            genion.Genion512(gppions_tpr, gio_gro, sol_top, gio_top).launch()
-        else:
-            gen = genion.Genion512(gppions_tpr, gio_gro, sol_top, gio_top)
-            gen2 = gen.launchPyCOMPSs(sol2, gro2)
+        gio = genion.Genion512(gppions_tpr, gio_gro, sol_top, gio_top)
+        gen2 = gio.launchPyCOMPSs(sol2, gro2)
 
         print 'step9: gppmin -- Preprocessing: Energy minimization'
         gppmin_path = cdir(mut_path, 'step9_gppmin')
@@ -168,22 +142,16 @@ def main():
         gppmin_mdp = opj(gppmin_path, prop['gppmin_mdp'])
         shutil.copy(opj(mdp_dir, prop['gppmin_mdp']), gppmin_mdp)
         gppmin_tpr = opj(gppmin_path, prop['gppmin_tpr'])
-        if not PyCOMPSs:
-            grompp.Grompp512(gppmin_mdp, gio_gro, gio_top, gppmin_tpr).launch()
-        else:
-            gro = grompp.Grompp512(gppmin_mdp, gio_gro, gio_top, gppmin_tpr, gmx_path=gmx_path)
-            gro3 = gro.launchPyCOMPSs(gen2)
+        gppmin = grompp.Grompp512(gppmin_mdp, gio_gro, gio_top, gppmin_tpr, gmx_path=gmx_path)
+        gro3 = gppmin.launchPyCOMPSs(gen2)
 
         print 'step10: mdmin -- Running: Energy minimization'
         mdmin_path = cdir(mut_path, 'step10_mdmin')
         mdmin_gro = opj(mdmin_path, prop['mdmin_gro'])
         mdmin_trr = opj(mdmin_path, prop['mdmin_trr'])
         mdmin_edr = opj(mdmin_path, prop['mdmin_edr'])
-        if not PyCOMPSs:
-            mdrun.Mdrun512(gppmin_tpr, mdmin_trr, mdmin_gro, mdmin_edr).launch()
-        else:
-            md = mdrun.Mdrun512(gppmin_tpr, mdmin_trr, mdmin_gro, mdmin_edr, gmx_path=gmx_path)
-            md1 = md.launchPyCOMPSs(gro3)
+        mdmin = mdrun.Mdrun512(gppmin_tpr, mdmin_trr, mdmin_gro, mdmin_edr, gmx_path=gmx_path)
+        md1 = mdmin.launchPyCOMPSs(gro3)
 
         print ('step11: gppnvt -- Preprocessing: nvt'
                'constant number of molecules, volume and temp')
@@ -192,11 +160,8 @@ def main():
         gppnvt_mdp = opj(gppnvt_path, prop['gppnvt_mdp'])
         shutil.copy(opj(mdp_dir, prop['gppnvt_mdp']), gppnvt_mdp)
         gppnvt_tpr = opj(gppnvt_path, prop['gppnvt_tpr'])
-        if not PyCOMPSs:
-            grompp.Grompp512(gppnvt_mdp, mdmin_gro, gio_top, gppnvt_tpr).launch()
-        else:
-            gppnvt = grompp.Grompp512(gppnvt_mdp, mdmin_gro, gio_top, gppnvt_tpr, gmx_path=gmx_path)
-            gppnvt1 = gppnvt.launchPyCOMPSs(gen2, md1)
+        gppnvt = grompp.Grompp512(gppnvt_mdp, mdmin_gro, gio_top, gppnvt_tpr, gmx_path=gmx_path)
+        gppnvt1 = gppnvt.launchPyCOMPSs(gen2, md1)
 
         print ('step12: mdnvt -- Running: nvt'
                'constant number of molecules, volume and temp')
@@ -205,13 +170,9 @@ def main():
         mdnvt_trr = opj(mdnvt_path, prop['mdnvt_trr'])
         mdnvt_edr = opj(mdnvt_path, prop['mdnvt_edr'])
         mdnvt_cpt = opj(mdnvt_path, prop['mdnvt_cpt'])
-        if not PyCOMPSs:
-            mdrun.Mdrun512(gppnvt_tpr, mdnvt_trr, mdnvt_gro,
-                           mdnvt_edr, output_cpt_path=mdnvt_cpt).launch()
-        else:
-            mdnvt = mdrun.Mdrun512(gppnvt_tpr, mdnvt_trr, mdnvt_gro,
-                           mdnvt_edr, output_cpt_path=mdnvt_cpt, gmx_path=gmx_path)
-            mdnvt1 = mdnvt.launchPyCOMPSs(gppnvt1)
+        mdnvt = mdrun.Mdrun512(gppnvt_tpr, mdnvt_trr, mdnvt_gro,
+                               mdnvt_edr, output_cpt_path=mdnvt_cpt, gmx_path=gmx_path)
+        mdnvt1 = mdnvt.launchPyCOMPSs(gppnvt1)
 
         print ('step13: gppnpt -- Preprocessing: npt'
                'constant number of molecules, pressure and temp')
@@ -220,13 +181,9 @@ def main():
         gppnpt_mdp = opj(gppnpt_path, prop['gppnpt_mdp'])
         shutil.copy(opj(mdp_dir, prop['gppnpt_mdp']), gppnpt_mdp)
         gppnpt_tpr = opj(gppnpt_path, prop['gppnpt_tpr'])
-        if not PyCOMPSs:
-            grompp.Grompp512(gppnpt_mdp, mdnvt_gro, gio_top,
-                             gppnpt_tpr, cpt_path=mdnvt_cpt).launch()
-        else:
-            gppnvt = grompp.Grompp512(gppnpt_mdp, mdnvt_gro, gio_top,
-                             gppnpt_tpr, cpt_path=mdnvt_cpt, gmx_path=gmx_path)
-            gppnvt2 = gppnvt.launchPyCOMPSs(gen2, mdnvt1)
+        gppnpt = grompp.Grompp512(gppnpt_mdp, mdnvt_gro, gio_top,
+                                  gppnpt_tpr, cpt_path=mdnvt_cpt, gmx_path=gmx_path)
+        gppnvt2 = gppnpt.launchPyCOMPSs(gen2, mdnvt1)
 
         print ('step14: mdnpt -- Running: npt'
                'constant number of molecules, pressure and temp')
@@ -235,13 +192,9 @@ def main():
         mdnpt_trr = opj(mdnpt_path, prop['mdnpt_trr'])
         mdnpt_edr = opj(mdnpt_path, prop['mdnpt_edr'])
         mdnpt_cpt = opj(mdnpt_path, prop['mdnpt_cpt'])
-        if not PyCOMPSs:
-            mdrun.Mdrun512(gppnpt_tpr, mdnpt_trr, mdnpt_gro,
-                           mdnpt_edr, output_cpt_path=mdnpt_cpt).launch()
-        else:
-            mdnpt = mdrun.Mdrun512(gppnpt_tpr, mdnpt_trr, mdnpt_gro,
-                                   mdnpt_edr, output_cpt_path=mdnpt_cpt, gmx_path=gmx_path)
-            mdnpt1 = mdnpt.launchPyCOMPSs(gppnvt2)
+        mdnpt = mdrun.Mdrun512(gppnpt_tpr, mdnpt_trr, mdnpt_gro,
+                               mdnpt_edr, output_cpt_path=mdnpt_cpt, gmx_path=gmx_path)
+        mdnpt1 = mdnpt.launchPyCOMPSs(gppnvt2)
 
         print 'step15: gppeq -- 1ns Molecular dynamics Equilibration'
         gppeq_path = cdir(mut_path, 'step15_gppeq')
@@ -249,13 +202,9 @@ def main():
         gppeq_mdp = opj(gppeq_path, prop['gppeq_mdp'])
         shutil.copy(opj(mdp_dir, prop['gppeq_mdp']), gppeq_mdp)
         gppeq_tpr = opj(gppeq_path, prop['gppeq_tpr'])
-        if not PyCOMPSs:
-            grompp.Grompp512(gppeq_mdp, mdnpt_gro, gio_top,
-                             gppeq_tpr, cpt_path=mdnpt_cpt).launch()
-        else:
-            gppeq = grompp.Grompp512(gppeq_mdp, mdnpt_gro, gio_top,
-                                     gppeq_tpr, cpt_path=mdnpt_cpt, gmx_path=gmx_path)
-            gppeq1 = gppeq.launchPyCOMPSs(gen2, mdnpt1)
+        gppeq = grompp.Grompp512(gppeq_mdp, mdnpt_gro, gio_top,
+                                 gppeq_tpr, cpt_path=mdnpt_cpt, gmx_path=gmx_path)
+        gppeq1 = gppeq.launchPyCOMPSs(gen2, mdnpt1)
 
         print 'step16: mdeq -- 1ns Molecular dynamics Equilibration'
         mdeq_path = cdir(mut_path, 'step14_mdeq')
@@ -263,14 +212,10 @@ def main():
         mdeq_trr = opj(mdeq_path, prop['mdeq_trr'])
         mdeq_edr = opj(mdeq_path, prop['mdeq_edr'])
         mdeq_cpt = opj(mdeq_path, prop['mdeq_cpt'])
-        if not PyCOMPSs:
-            mdrun.Mdrun512(gppeq_tpr, mdeq_trr, mdeq_gro,
-                           mdeq_edr, output_cpt_path=mdeq_cpt).launch()
-        else:
-            mdeq = mdrun.Mdrun512(gppeq_tpr, mdeq_trr, mdeq_gro,
-                                  mdeq_edr, output_cpt_path=mdeq_cpt, gmx_path=gmx_path)
-            mdeq1 = mdeq.launchPyCOMPSs(gppeq1)
-            mdeqList.append(mdeq1)
+        mdeq = mdrun.Mdrun512(gppeq_tpr, mdeq_trr, mdeq_gro,
+                              mdeq_edr, output_cpt_path=mdeq_cpt, gmx_path=gmx_path)
+        mdeq1 = mdeq.launchPyCOMPSs(gppeq1)
+        mdeqList.append(mdeq1)
 
         rmtemp()
         cont += 1
