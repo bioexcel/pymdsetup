77e428a9ebdf3e8f9ac298de7177d41df14ee604
Pau Andrio <pau.andrio@bsc.es>
2016-05-02 10:06:06 +0200

    launch in pycompss_dumies

--- launch/launch.sh
+++
@@ -1,1 +0,0 @@
-runcompss -d --lang=python --classpath=/home/compss/PyCOMPSs/git/pymdsetup/:/pymdsetup/ --comm=integratedtoolkit.nio.master.NIOAdaptor /home/compss/PyCOMPSs/git/pymdsetup/workflows/gromacs_full_pycompss.py
--- pycompss_dummies/launch.sh
+++ pycompss_dummies/launch.sh
@@ -1,1 +1,1 @@
-runcompss --lang=python --classpath=/home/compss/PyCOMPSs/git/pymdsetup:/pymdsetup -d -g --comm=integratedtoolkit.gat.master.GATAdaptor /home/compss/PyCOMPSs/git/pymdsetup/workflows/gromacs_full.py
+runcompss -d --lang=python --classpath=/home/compss/PyCOMPSs/git/pymdsetup/:/pymdsetup/ --comm=integratedtoolkit.nio.master.NIOAdaptor /home/compss/PyCOMPSs/git/pymdsetup/workflows/gromacs_full_pycompss.py
